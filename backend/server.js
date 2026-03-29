b63d2b2fbba55d517b658dba486dd353d680f4a8
Dare Ololade <oladare186@gmail.com>
2026-03-29 19:28:55 +0100

    fix cors origins

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const noteRouter = require("./routes/noteRoutes")
 dotenv.config();
 connectDB();
 const app = express();
-app.use(cors({
-  origin: ["https://notely-flow.vercel.app/login", "http://localhost:5173"],
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRouter)
 app.use("/api/note", noteRouter)
